0b76de85df1159658bdc3fd3e1b532ba310d97cb
RozzenRose <RozzenRose@gmail.com>
2026-03-03 16:49:45 +0100

    added transaction repository interfaces

---
+++ app/domain/interfaces/transaction_repository.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+from app.domain.entities.transaction import Transaction
+
+
+class ITransactionRepository(ABC):
+
+    @abstractmethod
+    async def create_transaction(self, transaction: Transaction) -> None:
+        pass
+
+    @abstractmethod
+    async def get_all_transaction(self) -> list[Transaction] | None:
+        pass
+
+    @abstractmethod
+    async def get_transaction_by_id(self, uuid: UUID) -> Transaction | None:
+        pass
