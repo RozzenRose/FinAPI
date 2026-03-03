328e0c73c96c79217700a28720744f1de90ec816
RozzenRose <RozzenRose@gmail.com>
2026-03-03 15:28:42 +0100

    cut of buisnes logic from CreateTransactionRequest

--- app/api/schemas/schemas_transaction.py
+++ app/api/schemas/schemas_transaction.py
@@ -15,16 +15,3 @@ class CreateTransactionRequest(BaseModel):
     description: str
     date: datetime
     entries: List[EntryItem]
-
-    # Дополнительно проверка бизнес-логики: сумма дебета == сумма кредита
-    @property
-    def total_debit(self) -> float:
-        return sum(e.amount for e in self.entries if e.type == EntryType.DEBIT)
-
-    @property
-    def total_credit(self) -> float:
-        return sum(e.amount for e in self.entries if e.type == EntryType.CREDIT)
-
-    def validate_balance(self):
-        if self.total_debit != self.total_credit:
-            raise ValueError("Total debit must equal total credit")
