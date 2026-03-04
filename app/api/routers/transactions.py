90ffba4a0c4fe474fc76a8b26fcd40a6985e3dcb
RozzenRose <RozzenRose@gmail.com>
2026-03-04 12:49:37 +0100

    GetTransactionById rout added

--- app/api/routers/transactions.py
+++ app/api/routers/transactions.py
@@ -1,9 +1,10 @@
 from typing import Annotated
+from uuid import UUID
 
 from fastapi import APIRouter, Depends
 from app.api.schemas.schemas_transaction import CreateTransactionRequest
-from app.services.usecases.transaction_usecases import CreateTransactionUseCase
-from app.infrastructure.di import get_create_transaction_usecase
+from app.services.usecases.transaction_usecases import CreateTransactionUseCase, GetTransactionByIdUseCase
+from app.infrastructure.di import get_create_transaction_usecase, get_transaction_by_id_usecase
 
 
 router = APIRouter(prefix='/api', tags=['api'])
@@ -15,3 +16,9 @@ async def create_transaction(request: CreateTransactionRequest,
     return await use_case.execute(description=request.description,
                                   date=request.date,
                                   raw_entries=request.entries)
+
+
+@router.get("/transactions/{id}")
+async def get_transaction_by_id(id: UUID,
+                                use_case: Annotated[GetTransactionByIdUseCase, Depends(get_transaction_by_id_usecase)]):
+    return await use_case.execute(id)
